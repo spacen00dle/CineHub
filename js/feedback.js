ceaddaf8a478ec06b6e62a220f7e89eb57bb28c9
waelAiden <38505172+waelaiden@users.noreply.github.com>
2019-02-15 23:27:22 -0500

    Add files via upload

---
+++ js/feedback.js
@@ -0,0 +1,29 @@
+  $(function() {
+    $('.error').hide();
+	
+    $(".button").click(function(){ 
+      // validate and process form here
+      
+      $('.error').hide();
+      var firstName = $("input#fname").val();
+     if (firstName == "") {
+		
+		  return false;	
+		}   
+		var lastName = $("input#lname").val();
+	 if (lastName == "") {
+		
+		  return false;
+		}     
+		var email = $("input#email").val();
+     if (email == "") {
+		
+          return false;
+	 }  
+	 	var feedback = $("textarea#subject").val();
+     if (feedback == "") {
+		
+          return false;
+	 }  
+    });
+  });
